4d1e4ba8f2a84a28d35be8a6da4db3b9f7d84f29
Pierre SOMNY <113438729+pierresomny@users.noreply.github.com>
2023-04-27 10:33:04 +0200

    edit: missing connectors and update of gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { ReactElement } from 'react';
 import classes from 'src/App.module.scss';
+import { NbaTeamSelector } from 'src/components/NbaTeamSelector.tsx';
+import { TeamCard } from 'src/components/TeamCard.tsx';
+import { useTrackedTeams } from 'src/hooks/trackedTeams.ts';
+import { Team } from 'src/utils/types.ts';
 
 export function App(): ReactElement {
+
+	const { trackedTeams } = useTrackedTeams();
 
 	return (
 		<div className={ classes.app }>
 			<h1>NBA Score Tracking App</h1>
+			<NbaTeamSelector/>
+			<div className={ classes.content }>
+				{ trackedTeams.map((team: Team, key: number) => <TeamCard key={ key } team={ team }/>) }
+			</div>
 		</div>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'src/App.tsx';
+import { TrackedTeamsProvider } from 'src/components/TrackedTeamsProvider.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<App/>
+		<TrackedTeamsProvider>
+			<App/>
+		</TrackedTeamsProvider>
 	</React.StrictMode>,
 );
